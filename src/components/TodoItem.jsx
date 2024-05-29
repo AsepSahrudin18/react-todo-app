fab74109887e227c30a5036fcdf87c21b95a3dc6
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2024-05-29 09:56:43 +0700

    Develop toggle todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,24 @@
 function TodoItem(props){
+    // Definisikan function getTodoTitleStyle di sini
+  const getTodoTitleStyle = () => {
+    if (props.todo.completed === true) {
+      return { textDecoration: 'line-through' }
+    } else {
+      return { textDecoration: 'none' }
+    }
+  }
     return (
         // <p>{props.title}</p>
         <div style={styles.todoItem}>
-            <p>{props.todo.title}</p>
+             {/* Menambahkan checkbox */}
+             <input 
+                type="checkbox" 
+                style={styles.checkbox} 
+                // Jalankan function toggleCompleted ketika checkbox di-klik
+                onChange={() => props.toggleCompleted(props.todo.id)}
+             />
+
+            <p style={getTodoTitleStyle()}>{props.todo.title}</p>
         </div>
     )
 }
@@ -11,6 +27,17 @@ const styles = {
     todoItem: {
       border: '2px solid #f4f4f4',
       fontSize: '24px',
+      // Tambahkan styles di bawah ini
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
+    // Tambahkan styles di bawah ini
+    checkbox: {
+        marginRight: '10px',
+        height: '18px',
+        width: '18px',
+    },
+    
   }
 export default TodoItem;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 function Todos(props){
-    
+
     return (
         <div style={styles.container}>
             <h1>This is Todos component</h1>
 
             {props.todos.map((todo) => {
                 // return <p key={todo.id}>{todo.title}</p>
-                return <TodoItem key={todo.id} todo={todo}/>
+                return <TodoItem key={todo.id} todo={todo} toggleCompleted={props.toggleCompleted}/>
             })}
         </div>
     )
